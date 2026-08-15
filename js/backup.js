565174f6a04768b53df31f1dd106fffefbad8642
behzadbagheri8757-sudo <behzad.bagheri8757@gmail.com>
2026-08-15 13:40:58 +0330

    Add files via upload

--- js/backup.js
+++ js/backup.js
@@ -257,10 +257,9 @@ function exportExcel(){
   XLSX.utils.book_append_sheet(wb, XLSX.utils.json_to_sheet(invRows.length?invRows:[{'شماره فاکتور':''}]), 'فاکتورها');
 
   const prodRows = data.products.map(p=>({
-    'نام کالا': p.name, 'دسته‌بندی': p.category||'', 'قیمت خرید (FIFO)': Math.round(productFifoUnitCost(p.id)),
-    'قیمت خرید (مبنای پیش‌فرض)': p.buy,
+    'نام کالا': p.name, 'دسته‌بندی': p.category||'', 'قیمت خرید': p.buy,
     'قیمت عمده': p.wholesale, 'قیمت مصرف‌کننده': p.retail, 'موجودی': p.stockQty,
-    'ارزش ریالی موجودی (FIFO)': Math.round(productInventoryValue(p.id)),
+    'ارزش ریالی موجودی': (p.stockQty||0)*(p.buy||0),
   }));
   XLSX.utils.book_append_sheet(wb, XLSX.utils.json_to_sheet(prodRows.length?prodRows:[{'نام کالا':''}]), 'کالاها');
 
